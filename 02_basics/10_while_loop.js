b213a5dd9957c1ad501f0f82e68462192d99021b
Surjeet Verma <vermasurjeetskv@gmail.com>
2026-08-11 17:56:58 +0000

    while loop done

---
+++ 02_basics/10_while_loop.js
@@ -0,0 +1,27 @@
+
+let index = 0;
+while (index<=10) {
+    console.log(index)
+    index = index + 2
+}
+
+
+let arr = ["Nick", "jack", "Krish", "John"]
+
+let i = 0
+while(i<arr.length){
+    console.log(arr[i])
+    i++
+}
+
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`)
+    score++
+    
+} while (score<=10); 
+
+// Output : Score is 11
+
